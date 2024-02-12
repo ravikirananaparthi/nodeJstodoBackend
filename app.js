4272e598af788d7a5fd3809923111fc34fc1c6b8
ravikirananaparthi <anaparthiravikiran20037@gmail.com>
2024-02-12 18:40:55 +0530

    fixed engine

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
