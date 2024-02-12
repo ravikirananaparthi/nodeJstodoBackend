28d47953bbc2d52e2541f32df0073b670d0aa920
ravikirananaparthi <anaparthiravikiran20037@gmail.com>
2024-02-12 18:54:57 +0530

    fixed engine

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ config({
 });
 
 //using middleware
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: '*',
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
